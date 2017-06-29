0c4e31ef351d7e077d2b04eebfc1bd5a55fea4ed
Alexis Arnaldo Quiñonez <alexis@gmail.com>
2017-06-29 14:02:40 -0300

    Consulta sql

--- login/php/login_procesar.php
+++ login/php/login_procesar.php
@@ -8,20 +8,6 @@
         $password =  $_POST['pass'];
 
         //VERIFICAMOS EN LA BD SI EXISTE
-        /*$sql1= "select * from user where (username=\"$_POST[username]\" or email=\"$_POST[username]\") and password=\"$_POST[password]\" ";
-			$query = $con->query($sql1);
-			while ($r=$query->fetch_array()) {
-				$user_id=$r["id"];
-				break;
-			}
-			if($user_id==null){
-				print "<script>alert(\"Acceso invalido.\");window.location='../login.php';</script>";
-			}else{
-				session_start();
-				$_SESSION["user_id"]=$user_id;
-				print "<script>window.location='../home.php';</script>";				
-			}*/
-
         $sql = "SELECT * FROM `login` WHERE login_correo ='$usuario' AND login_password = '$password'";    
         $query = $conexion->query($sql);
         //recorremos y buscamos al usuario en la tabla login que armo el CAPO DAVID
