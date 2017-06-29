c29215b0dbaf67417a90d2839fc973feb47f7a0a
davidalejandrolerea@gmail.com <3548829935488299>
2017-06-29 17:12:24 -0300

    no message

--- login/php/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("myDBC.php");
-$consultas=new myDBC();
-$user=trim($_POST['usuario']);
-$pass=trim($_POST['contrasenia']);
-$log = $consultas->logueo($user, $pass);
-
-?>
--- login/php/salir.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("myDBC.php");
-session_destroy();
-header("Location: http://localhost/login/index.php");
-?>
